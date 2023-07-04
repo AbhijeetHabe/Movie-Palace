4af4a4387d30716d3b2c161fb780a0c8a88cdf01
AbhijeetHabe <abhijeethabe23@gmail.com>
2023-07-04 18:12:40 +0530

    Adding movie palace project

---
+++ movies/movies/src/main/java/com/cdac/movies/service/MovieService.java
@@ -0,0 +1,24 @@
+package com.cdac.movies.service;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.cdac.movies.pojo.Movie;
+import com.cdac.movies.repository.MovieRepository;
+
+@Service
+public class MovieService {
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	public List<Movie> allMovies() {
+		return movieRepository.findAll();
+	}
+	
+	public Optional<Movie> singleMovie(String id) {
+		return movieRepository.findMovieByImdbId(id);
+	}
+}
